22d877d5896c24e08b7ba287eac969261e9f96eb
laurakelly <llkelly88@gmail.com>
2014-03-10 02:11:37 -0700

    removing unused click event functions

--- static/resume/script.js
+++ static/resume/script.js
@@ -167,29 +167,11 @@ function verticalCenter () {
 
         $ul.css("margin-top", (135 - height) / 2 + "px")
     })
-
 }
-
-/* 
- * function to create click event for d3 elements
- * jQuery's usual .click() doesn't work with d3 elements
- * http://stackoverflow.com/a/11180172/1880100 
- */
-jQuery.fn.d3Click = function () {
-  this.each(function (i, e) {
-    var evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-
-    e.dispatchEvent(evt);
-  });
-};
 
 $(document).ready(function () {
     timeline();
     bubbleChart();
-
-    // Expand WebAction by default
-    $("#experience [data-title='WebAction'].dot").d3Click();
 
     // Vertical center project descriptions
     verticalCenter();
