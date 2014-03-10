54044546bb754bb573350eae66619a7f7bd544b7
laurakelly <llkelly88@gmail.com>
2014-03-10 01:36:56 -0700

    adding D3 resume

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ def women2():
 def neuron():
     return redirect('http://www.ncbi.nlm.nih.gov/pubmed/22153366')
 
+@app.route('/resume')
+def resume():
+    return render_template("d3_resume.html")
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
---
+++ static/resume/script.js
@@ -0,0 +1,202 @@
+function bubbleChart() {
+    var diameter = 700,
+        format = d3.format(",d"),
+        color = d3.scale.category20();
+
+    var bubble = d3.layout.pack()
+        .sort(null)
+        .size([diameter, diameter])
+        .padding(1.5);
+
+    var svg = d3.select("#skills").append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .attr("class", "bubble");
+
+    d3.json("static/resume/data.json", function(error, root) {
+      var node = svg.selectAll(".node")
+          .data(bubble.nodes(classes(root))
+          .filter(function(d) { return !d.children; }))
+        .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+      node.append("title")
+          .text(function(d) { return d.className + ": " + format(d.value); });
+
+      node.append("circle")
+          .attr("r", function(d) { return d.r; })
+          .style("fill", function(d) { 
+              if (typeof d.fill != 'undefined') return d.fill;
+              return color(d.packageName); });
+
+      node.append("text")
+          .attr("dy", ".3em")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d.className.substring(0, d.r / 3); });
+    });
+}
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+function classes(root) {
+  var classes = [];
+
+  function recurse(name, node) {
+    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+    else classes.push({packageName: name, className: node.name, value: node.size, fill: node.fill});
+  }
+
+  recurse(null, root);
+  return {children: classes};
+}
+
+function timeline () {
+    var data = undefined;
+
+    d3.json("static/resume/experience.json", function(error, json) {
+        if (error) return console.warn(error);
+        data = json;
+
+        var margin = {top: 50, right: 10, bottom: 20, left: 10},
+            width = 960 - margin.left - margin.right,
+            height = 700 - margin.top - margin.bottom;
+
+        var parseDate = d3.time.format("%d-%b-%y").parse;
+
+        var svg = d3.select("#experience").append("svg")
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("width", width + margin.left + margin.right)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");;
+
+        var y = d3.time.scale()
+            .domain([new Date().getTime(), 1136094769000])
+            .range([0, height]);
+
+        var yPlot = function(d) { return y(d.date);}
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(9)
+            .tickFormat(function(d){return d3.time.format('%Y')(new Date(d));});
+
+        var line = d3.svg.line()
+            .y(function(d) { return y(d.date) });
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(480, 0)")
+            .call(yAxis);
+
+        svg.append("g")
+            .attr("transform", "translate(480, 0)")
+            .selectAll(".dot")
+            .data(data)
+          .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 5)
+            .attr("cy", yPlot)
+            .attr("data-title", function (d) { return d.title })
+            .on("click", function (d) {
+                tooltip(d, "#experience");
+            });
+      
+        for (var i=0; i < Object.keys(data).length; i++) {
+            var d = data[Object.keys(data)[i]];
+            tooltip(d, "#experience" );
+        }
+    });
+}
+
+function tooltip (d, id) {
+    $(id).append(tooltipText(d));
+    $(id + " .tooltip[data-title='" + d.title + "']").css(tooltipLoc(d, id));
+}
+
+function tooltipText (d) {
+    var text = "<div class='tooltip " + d.orient.horizontal + "' data-title='" + d.title + "'>";
+    text = text + "<h3>" + d.title + "</h3>";
+    text = text + "<h4>" + d.role + "</h4><ul>"
+
+    for (var i=0; i < d.text.length; i++) {
+        text = text + "<li>" + d.text[i] + "</li>";
+    };
+    
+    return text + "</ul>"
+}
+
+function tooltipLoc (d, id) {
+    var $el = $(id + " circle[data-title='" + d.title + "'].dot"),
+        position = $el.position();
+
+    if (d.orient.horizontal === "left") {
+        var left = position.left - 450 - 20;
+    } else {
+        var left = position.left + 30;
+    }
+
+    if (d.orient.vertical === "top") {
+        var top = position.top - 40;
+    } else {
+        var top = position.top - d.orient.height;
+    }
+
+    return { "left": left, "top": top }
+
+}
+
+function closeup ($el) {
+
+    // remove any existing closeup
+    $(".detail").remove();
+
+    url = $el.attr("src").replace("thumbnail", "detail"); 
+    $("<img src='" + url + "' class='detail'>").insertBefore("#gallery");
+
+    // scroll to the bottom
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+
+}
+
+function verticalCenter () {
+
+    $("#projects li > ul").each(function () {
+        var $ul = $(this),
+            height = $ul.height();
+
+        $ul.css("margin-top", (135 - height) / 2 + "px")
+    })
+
+}
+
+/* 
+ * function to create click event for d3 elements
+ * jQuery's usual .click() doesn't work with d3 elements
+ * http://stackoverflow.com/a/11180172/1880100 
+ */
+jQuery.fn.d3Click = function () {
+  this.each(function (i, e) {
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+    e.dispatchEvent(evt);
+  });
+};
+
+$(document).ready(function () {
+    timeline();
+    bubbleChart();
+
+    // Expand WebAction by default
+    $("#experience [data-title='WebAction'].dot").d3Click();
+
+    // Vertical center project descriptions
+    verticalCenter();
+
+    // Show gallery image when clicked
+    $("#gallery img").click(function () {
+        closeup($(this));
+    });
+
+});
