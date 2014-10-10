edf4b228f8c5c704c17d27374cdfb54d4ffa0b21
Laura Kelly <laurakelly135@gmail.com>
2014-10-09 23:03:49 -0700

    adding port back in

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ def women2():
 def neuron():
     return redirect('http://www.ncbi.nlm.nih.gov/pubmed/22153366')
 
-if __name__== "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
